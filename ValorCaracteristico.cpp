f42e93d8719d4655dd98d2ec2c1172bfaa62ab9a
HatWithPins <daniel.carril4@gmail.com>
2023-11-11 16:55:19 +0100

    Add project files.

---
+++ ValorCaracteristico.cpp
@@ -0,0 +1,12 @@
+﻿// ValorCaracteristico.cpp : Defines the entry point for the application.
+//
+
+#include "ValorCaracteristico.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
